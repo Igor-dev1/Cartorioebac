3efdfa6dab6e089bc5274c302484085fbfeec118
Igor Dev <74027430+Igor-dev1@users.noreply.github.com>
2023-09-24 15:09:04 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- Cartório EBAC.c
+++ Cartório EBAC.c
@@ -133,7 +133,8 @@ int main()
    
         printf("\t1- Registrar nomes\n");
         printf("\t2- Consultar nomes\n");
-        printf("\t3- Deletar nomes\n\n"); 
+        printf("\t3- Deletar nomes\n"); 
+        printf("\t4- Sair do sistema\n\n");
         printf(" Escolha a opção que deseja:");
    
         scanf("%d", &opcao); //armezenando o input do usuario ou escolha do usuario
@@ -152,6 +153,11 @@ int main()
    	    
    	        case 3:
         	deletar(); // chamada de função
+   	        break;
+   	        
+   	        case 4:
+   	        printf("Obrigado por utilizar o sistema");
+   	        return 0;
    	        break;
    	    
    	        default:
